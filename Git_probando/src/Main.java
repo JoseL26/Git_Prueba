168e4dcc59b441240c1d7cc1fa76b16f6bf21e35
JoseL26 <63889036+JoseL26@users.noreply.github.com>
2022-10-27 03:25:36 -0500

    Update Main.java
    editado desde la web

--- Git_probando/src/Main.java
+++ Git_probando/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("modificando el main");
+        
+        int num = 10;
     }
 }
